76b557e6c0260aa028b22396a7aecadb16eea26b
@ElPrinz <anishammami2010@hotmail.fr>
2020-05-23 00:11:58 +0200

    Added mouse click ability to drag UI
    Instantiate a scene called Farm
    Added background image (Berg, Mountain, Rocks)
    Added fields for plants

---
+++ Assets/CYCLE/NET_DragLogic.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NET_DragLogic : MonoBehaviour
+{
+
+    public struct userAttributes { }
+    public struct appAttributes { }
+    public PlantsType currentPlant;
+    public PlantsTime currentPlantTime;
+    public bool needWater = false;
+    public Sprite[] listPlantsTime;
+    public GameObject prefabsType;
+
+    [Serializable]
+    public enum PlantsType
+    {
+        NONE,
+        FRUITS,
+        FLOWERS,
+        VEGETABLES,
+
+    }
+
+    public enum PlantsTime
+    {
+        NONE,
+        SEED,
+        PETIT,
+        READY,
+        DEAD,
+
+    }
+
+    public static NET_DragLogic Instance { set; get; }
+    void Start()
+    {
+        Instance = this;
+        //currentPlant = PlantsType.NONE;
+        currentPlantTime = PlantsTime.NONE;
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void SetDrag(GameObject oben, GameObject unten )
+    {
+        //GameObject.Destroy(oben);
+        // if != zero, keep seed in hand, if zero destroy
+        prefabsType = GameObject.Find("FlowerPrefab");
+        Vector3 newPos = new Vector3(unten.transform.position.x, unten.transform.position.y +2, unten.transform.position.z);
+        var go = Instantiate(prefabsType, newPos, Quaternion.identity); // 0 = FlowerPrefab
+        go.transform.localScale = new Vector3(1f, 1f, go.transform.localScale.z);
+        go.transform.parent = unten.transform;
+
+        currentPlantTime = PlantsTime.SEED;
+        StartCoroutine(GrowSystem(currentPlantTime));
+    }
+
+
+    IEnumerator GrowSystem(PlantsTime time)
+    {
+        switch (time.ToString())
+        {
+            case "SEED":
+                this.gameObject.GetComponent<SVGImage>().sprite = listPlantsTime[0];
+                yield return new WaitForSeconds(2);
+                currentPlantTime = PlantsTime.PETIT;
+                this.gameObject.GetComponent<SVGImage>().sprite = listPlantsTime[1];
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y +5, this.transform.position.z);
+                yield return new WaitForSeconds(2);
+                currentPlantTime = PlantsTime.READY;
+                this.gameObject.GetComponent<SVGImage>().sprite = listPlantsTime[2];
+                this.transform.localScale = new Vector3(this.transform.localScale.x + 0.3f, this.transform.localScale.y + 0.3f, this.transform.localScale.z);
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z);
+                yield return new WaitForSeconds(2);
+                currentPlantTime = PlantsTime.DEAD;
+                this.gameObject.GetComponent<SVGImage>().sprite = listPlantsTime[3];
+                break;
+
+
+            default:
+                break;
+        }
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        
+        //yield return new WaitForSeconds(5);
+
+        
+       // Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
---
+++ Assets/Scripts/UIElementDragger.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIElementDragger : MonoBehaviour
+{
+    public const string DRAGGABLE_TAG = "UIDraggable";
+
+    private bool dragging = false;
+
+    private Vector2 originalPosition;
+
+    private Transform objectToDrag;
+    private SVGImage objectToDragImage;
+    public ToolTypes tooltype = ToolTypes.NONE;
+    public FootTypes fooltype = FootTypes.NONE;
+
+    List<RaycastResult> hitObjects = new List<RaycastResult>();
+
+    public enum ToolTypes
+    {
+        NONE,
+        SEED,
+        GLOVES,
+
+    }
+    public enum FootTypes
+    {
+        NONE,
+        FIELD,
+
+    }
+
+
+    #region Monobehaviour API
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // press Mouse0
+        {
+            objectToDrag = GetDraggableTransformUnderMouse(); // select it
+
+            if(objectToDrag != null)
+            {
+                dragging = true;
+
+                objectToDrag.SetAsLastSibling();
+
+                originalPosition = objectToDrag.position;
+                objectToDragImage = objectToDrag.GetComponent<SVGImage>();
+                objectToDragImage.raycastTarget = false;
+            }
+        }
+
+        if (dragging)
+        {
+            objectToDrag.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0)) // release Mouse0
+        {
+            if(objectToDrag != null)
+            {
+                Transform objectToReplace = GetDraggableTransformUnderMouse();
+
+                
+
+                if (objectToReplace != null)
+                {
+                    switch (objectToReplace.name)
+                    {
+                        case "Field":
+                            Debug.Log("Put on Field");
+                            fooltype = FootTypes.FIELD;
+                            NET_DragLogic.Instance.SetDrag(objectToDrag.gameObject,objectToReplace.gameObject);
+                            break;
+                        default:
+                            Debug.LogError("Put on Unkown");
+                            fooltype = FootTypes.NONE;
+                            break;
+
+                    }
+                    if(tooltype == ToolTypes.SEED && fooltype == FootTypes.FIELD)
+                    {
+                        GameObject go = objectToReplace.GetChild(0).gameObject;
+                        objectToDrag.position = go.transform.position;
+                        Debug.Log("Put SEED on Field done!");
+                        fooltype = FootTypes.NONE;
+                        tooltype = ToolTypes.NONE;
+                        // lunch grow
+                    }
+                    /*
+                    objectToDrag.position = objectToReplace.position;
+                    objectToReplace.position = originalPosition;
+                    */
+                } else
+                {
+                    Debug.LogError("Put on Nothing");
+                    fooltype = FootTypes.NONE;
+
+                    objectToDrag.position = originalPosition; // push back
+                    //objectToDrag.position = Input.mousePosition; // put anywhere
+                }
+
+                objectToDragImage.raycastTarget = true;
+                objectToDrag = null;
+            }
+
+            dragging = false;
+        }
+    }
+    #endregion
+
+    private GameObject GetObjectUnderMouse()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+
+        pointer.position = Input.mousePosition;
+
+        EventSystem.current.RaycastAll(pointer, hitObjects);
+
+        if (hitObjects.Count <= 0) return null;
+
+        return hitObjects.First().gameObject;
+    }
+
+    private Transform GetDraggableTransformUnderMouse()
+    {
+        GameObject clickedObject = GetObjectUnderMouse();
+        Debug.Log(clickedObject.name);
+        switch (clickedObject.name)
+        {
+            case "SEED":
+                Debug.LogError("Seed Collected");
+                tooltype = ToolTypes.SEED;
+                break;
+            case "Field":
+                if(tooltype == ToolTypes.NONE)
+                {
+                    clickedObject = null;                               
+                }
+                Debug.LogError("Field NOT Collected");
+                fooltype = FootTypes.FIELD;
+                break;
+            case "FlowerPrefab(Clone)":
+                if (tooltype == ToolTypes.NONE)
+                {
+                    clickedObject = null;
+                }
+                Debug.LogError("Grown Plants NOT Collected");
+                fooltype = FootTypes.NONE;
+                break;
+            case "GLOVES":
+                Debug.LogError("Gloves Collected");
+                tooltype = ToolTypes.GLOVES;
+                break;
+            case "BAGSEED":
+                Debug.LogError("SEED BAG Opened");
+                tooltype = ToolTypes.NONE;
+                clickedObject = null;
+                break;
+
+            default:
+                Debug.LogError("unkownen Collected");
+                tooltype  = ToolTypes.NONE;
+                break;
+
+        }
+
+        if(clickedObject != null && clickedObject.tag == DRAGGABLE_TAG)
+        {
+            return clickedObject.transform;
+        }
+
+        return null;
+    }
+}
